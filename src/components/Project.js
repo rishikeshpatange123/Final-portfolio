71f97e263f5cfa6bacf52eeb30b717f5b424ce1c
rishikeshpatange123 <95735385+rishikeshpatange123@users.noreply.github.com>
2021-12-09 20:56:36 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,9 +7,9 @@ function Project() {
     return (
         <div className="Project-container">
             <div className="project">
-                <motion.img initial={{scale:0}} animate={{scale:1}} exit={{scale:0}} transition={{duration:0.5}} src="./images/PROJECT3.png" alt="#" />
+                <motion.img initial={{scale:0}} animate={{scale:1}} exit={{scale:0}} transition={{duration:0.5}} src="./Images/PROJECT3.png" alt="#" />
                 <div className="project1">
-                    <motion.img initial={{scale:0}} animate={{scale:1}} exit={{scale:0}} transition={{duration:0.5}} src="./images/project 1.png" alt="#" />
+                    <motion.img initial={{scale:0}} animate={{scale:1}} exit={{scale:0}} transition={{duration:0.5}} src="./Images/project 1.png" alt="#" />
                     <h1>More project coming soon...</h1>
                 </div>
             </div>
